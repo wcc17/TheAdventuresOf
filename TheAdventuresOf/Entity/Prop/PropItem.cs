6c0a3c1107c6adb8faf829d0cd4f9ae113fd6a9f
Christian <christian.curry17@gmail.com>
2018-01-09 23:15:32 -0500

    started progress on pricing for items and progression for the sword upgrade in SpecialLevelInfo.xml and XmlManager

---
+++ TheAdventuresOf/Entity/Prop/PropItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+/**
+ * Not the same as Item.cs. This is essentially a prop with a little extra info
+ **/
+namespace TheAdventuresOf
+{
+    public class PropItem : Prop
+    {
+        string name;
+        int? cost;
+
+        public PropItem(string name, int? cost = null) : base()
+        {
+            this.name = name;
+            this.cost = cost;
+        }
+    }
+}
