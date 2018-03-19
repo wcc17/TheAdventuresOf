ee58ee35c8e3e9ce926cc4eed7dc459e85ffb484
Christian Curry <christian.curry17@gmail.com>
2018-03-18 21:48:57 -0400

    Version 1.2.0

--- TheAdventuresOf/TheAdventuresOf.cs
+++ TheAdventuresOf/TheAdventuresOf.cs
@@ -11,7 +11,7 @@ namespace TheAdventuresOf
 	{
         public static bool showMouse;
 
-        public static bool showDebug = true; //set to false to turn everything else off
+        public static bool showDebug = false; //set to false to turn everything else off
         public static bool giveCoins = true;
         public static bool quickVictory = true;
         public static bool lowHealth = false;
